8f3388f0e3f8535828d9bf5d453dca98686b0d5a
Saunakatti <onni.veranen@proton.me>
2026-07-10 00:03:16 +0300

    Large commit - lots of features added - almost like a version 1.0 of the site

--- README.md
+++ README.md
@@ -1,52 +1,45 @@
 # Saunakatti's Personal Site
 
 My personal site for links and other stuff. Intended as a learning project for css, html etc.
-Uses Mocha pallette of [Catppuccin](https://github.com/catppuccin/catppuccin) for theming. The site also uses (or at least tries to use) [Nerd Fonts](https://github.com/ryanoasis/nerd-fonts) for cool icons. 
+Uses Mocha pallette of [Catppuccin](https://github.com/catppuccin/catppuccin) for the default theme. The site also uses (or at least tries to use) [Nerd Fonts](https://github.com/ryanoasis/nerd-fonts) for cool icons. 
 
 > [!Note]
 > GitHub pages means no backend.
 
 ### [saunakatti.github.io](https://saunakatti.github.io/)
 
-<img src="assets/images/preview1_desktop.png" alt="Desktop Prewiev 1" width="1000" height="510">
-<img src="assets/images/preview1_mobile.jpg" alt="Mobile Prewiev 1" width="300" height="650">
-
 ---
 
 ### List of ideas
+
 - Some small vague personal About and about the site.
 
-- (Done partially, TODO: import file from repo, change old ones to class names)
-  Rework usage of icons by importing "https://www.nerdfonts.com/assets/css/webfont.css"
-   - This would help when looking at
-    code without a Nerd Font
+- Move about site to documentation
+
+- (Done partially, TODO: import file from repo, change old ones to class names) Icons
 
 - Take inspo from indie websites
 
-- Add blog with recent post(s) on homepage
-
-- fix fonts
-
-- Make good codeblocks with prismjs
+- (partially done) fix fonts
 
 - Major cleanup at some point
   - Make IDs and Classes more understandable and remove unneeded ones
   - Remove pointless configuraton
   - Split into sections and add more comments
 
-- (Added, light theme might change) Add dark/light mode switch with latte flavour as light theme
+- organize files
 
-- (Future) Add picture gallery
+- sitemap
+
+- (Future) Add gallery
 
 - (Future) Add more animations
 
-- (Future) Add library for read books to force me to read more
-
 - (Future) Add no javascript mode for users not using javascript.
 
-<!-- gaming related stuff, smp??, fsmp blog? -->
+<!-- - (Future) Add library for read books to force me to read more  -->
 
-<!-- Could use Atabook to add a guestbook?>
+<!-- gaming related stuff, smp??, fsmp blog? -->
 
 <!-- Make a button 88 x 31>
 
--- scripts/lightmode.js
+++ scripts/lightmode.js
@@ -7,54 +7,79 @@
     Based on https://www.youtube.com/watch?v=_gKEUYarehE and
     https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_iframe_contentdocument
 
-    Adds class "lighmode" to body of document. The class contains color variables in style.css.
-    These overide the root colors defined in style.css. The order of blocks matters. 
+    Idk how I managed to do the iframe stuff. The order of blocks matters. 
 */
 
 
 /* set some variables */
 let lightmode = localStorage.getItem("lightmode")                       
 const themeSwitch = document.getElementById("theme-switch")
-const iframe = document.getElementById("iframe")
+const codeblock = document.getElementById("codeblock")
+const prismjsTheme = document.getElementById("prismjs-theme")
+const iframe = document.getElementById("iframe");
 
-/* iframe theme changing */
+
+
+/* iframe theme changing, these are basically the same as normal theme changing 
+but done in the iframe page */
+
 const EnableIframeLightmode = () => {
- /* var iframe = document.getElementById("iframe"); */
-  var ogfile = (iframe.contentWindow || iframe.contentDocument);
-  if (ogfile.document)ogfile = ogfile.document;
-  ogfile.body.classList.add("lightmode")
+   /* var iframe = document.getElementById("iframe"); */
+    var innerWindow = iframe.contentWindow;
+    if (innerWindow.document)innerWindow = innerWindow.document;
+    innerWindow.body.classList.add("lightmode");                        
+
+    var innerWindowPrismjs = innerWindow.getElementById("prismjs-theme")    
+    innerWindowPrismjs.href = "/assets/prismjs-themes/light-custom.css"       
+
 }
 
 const DisableIframeLightmode = () => {
- /* var iframe = document.getElementById("iframe"); */
-  var ogfile = (iframe.contentWindow || iframe.contentDocument);
-  if (ogfile.document)ogfile = ogfile.document;
-  ogfile.body.classList.remove("lightmode")
+   /* var iframe = document.getElementById("iframe"); */
+    var innerWindow = iframe.contentWindow;
+    if (innerWindow.document)innerWindow = innerWindow.document;
+    innerWindow.body.classList.remove("lightmode");                 
+
+    var innerWindowPrismjs = innerWindow.getElementById("prismjs-theme")
+    innerWindowPrismjs.href = "/assets/prismjs-themes/mocha.css"
+
 }
 
 /* theme changing */
-const enableLightmode = () => {
-    document.body.classList.add('lightmode')      
-    localStorage.setItem('lightmode', 'active')
 
-    if (document.body.contains(iframe)) {           /* These conditions check if page contains iframe. If it tried to execute whitout an iframe in page, script would break. */
-    EnableIframeLightmode()
+const enableLightmode = () => {
+    document.body.classList.add('lightmode')                    
+    localStorage.setItem('lightmode', 'active')                 /* saves lightmode status to localstorage  */
+
+    prismjsTheme.href = "/assets/prismjs-themes/light-custom.css"  /* Sets prismjs theme */
+
+    if (document.body.contains(iframe)) {           /* These conditions check if page contains iframe. If it tried to execute whitout an iframe in page, */
+    EnableIframeLightmode()                         /* script would break or atleast result in unwanted errors */
     } else {return}
+
+                            
 }
 
 const disableLightmode = () => {
     document.body.classList.remove('lightmode')
     localStorage.setItem('lightmode', null)
 
+    prismjsTheme.href = "/assets/prismjs-themes/mocha.css"
+
     if (document.body.contains(iframe)) {
     DisableIframeLightmode()
     } else {return}
+
 }
 
-if(lightmode === "active") enableLightmode()    /* If lightmode active in localstorage, enable without any interaction */
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("content loaded")
+    if(lightmode === "active") enableLightmode()                  /* If lightmode active in localstorage, enable without any interaction */
+});                                                             /* Waits for page html to load becouse if iframe isn't loaded when this runs, the script breaks  */
+    
 
 /* event listener for element "theme-switch" */
-themeSwitch.addEventListener("click", () => {                           /* if lightmode in localstorage is active; disable it and vise-versa */
+themeSwitch.addEventListener("click", () => {                           /* if lightmode in localstorage is active; disable it on click and vise-versa */
     lightmode = localStorage.getItem('lightmode')
     lightmode !== "active" ? enableLightmode() : disableLightmode() 
 })
